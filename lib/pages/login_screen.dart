3fc0793a37e1d99803d429b1367d32f98f82d971
quanming <116425066+wahpiangle@users.noreply.github.com>
2023-10-18 18:09:04 +0800

    finish 1st login/signup page

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:group_project/pages/auth/email_password_login.dart';
+import 'package:group_project/pages/auth/register_screen.dart';
 import 'package:group_project/pages/profile_screen.dart';
 import 'package:group_project/services/auth_service.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:group_project/main.dart';
+import 'package:magic_text/magic_text.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -34,25 +37,163 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            try {
-              await AuthService().signInWithGoogle();
-            } catch (e) {
-              if (mounted) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(e.toString()),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/background.png'),
+          ),
+        ),
+        constraints: const BoxConstraints.expand(),
+        child: Padding(
+          padding: const EdgeInsets.all(35.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Fitness\nUnleashed,',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                margin: const EdgeInsets.only(left: 120),
+                child: MagicText(
+                  "Socially\nEnhanced",
+                  breakWordCharacter: "-",
+                  minFontSize: 24,
+                  maxFontSize: 40,
+                  smartSizeMode: true,
+                  asyncMode: true,
+                  textStyle: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
-                );
-              }
-            }
-          },
-          child: const Text('Google login'),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton.icon(
+                  label: const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 12, 0, 12),
+                    child: Text(
+                      'Sign in with Google',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  onPressed: () async {
+                    try {
+                      await AuthService().signInWithGoogle();
+                    } catch (e) {
+                      if (mounted) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(e.toString()),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                  icon: Image.asset(
+                    'assets/google.png',
+                    height: 30,
+                    width: 30,
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(const Color(0xffe1f0cf)),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterScreen(),
+                          ),
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(const Color(0xff1a1a1a)),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        side: MaterialStateProperty.all(
+                          const BorderSide(color: Color(0xffc1c1c1)),
+                        ),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.fromLTRB(40, 12, 40, 12),
+                        child: Text(
+                          'Register',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const EmailPasswordLogin(),
+                          ),
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(const Color(0xff1a1a1a)),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        side: MaterialStateProperty.all(
+                          const BorderSide(color: Color(0xffc1c1c1)),
+                        ),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.fromLTRB(40, 12, 40, 12),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
