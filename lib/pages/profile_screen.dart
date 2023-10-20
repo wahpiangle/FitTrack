305ff7ef37654d1d616b596acb2286d7ac5c2122
stephanynene <110772101+stephanynene@users.noreply.github.com>
2023-10-20 14:09:36 +0800

    app bar and start workout button

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -12,7 +12,20 @@ class ProfileScreen extends StatelessWidget {
     final fullName = user?.userMetadata?['full_name'];
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile'),
+        backgroundColor: Color(0xFF1A1A1A),
+        title: Center(child: const
+        Text(
+            'Home',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        )
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          //Will lead to Search friends page
+          onPressed: () {},
+        ),
         actions: [
           TextButton(
             onPressed: () async {
@@ -27,27 +40,58 @@ class ProfileScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            if (profileImageUrl != null)
-              ClipOval(
-                child: Image.network(
-                  profileImageUrl,
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
+      body: Container(
+        color: Color(0xFF1A1A1A),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (profileImageUrl != null)
+                ClipOval(
+                  child: Image.network(
+                    profileImageUrl,
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              const SizedBox(height: 15),
+              Text(
+                fullName ?? '',
+              ),
+              //const SizedBox(height: 32),
+              //Start an Empty Widget
+              Padding(
+                padding: EdgeInsets.only(bottom: 100),
+                child: Container(
+                  width: 300,
+                  height: 60,
+                  child: TextButton(
+                    onPressed: (){},
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Color(0xFFC1C1C1)),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30), // Set the border radius to make it round
+                          ),
+                        ),
+                      ),
+                      child: Text(
+                          "Start an empty workout",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF1A1A1A),
+                          fontSize: 20,
+                        ),
+                      ),
+                  ),
                 ),
               ),
-            const SizedBox(height: 16),
-            Text(
-              fullName ?? '',
-            ),
-            const SizedBox(height: 32),
-          ],
-        ),
-      ),
+              //End of Start a widget
+      ]
+                ),
+    ),
+    ),
     );
   }
 }
