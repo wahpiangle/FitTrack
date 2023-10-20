b2e52c5843b52311c8e38844ea049ea2f738cd79
quanming <116425066+wahpiangle@users.noreply.github.com>
2023-10-20 23:23:45 +0800

    remove unused packages

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:group_project/services/database_service.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,29 +10,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    // TODO this is just an example of loading sqlite data into a listview
     return Scaffold(
-      body: Center(
-          child: FutureBuilder(
-        future: DatabaseService()
-            .getWorkouts(), //! this function will not work as the table is not created yet
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            print(snapshot.data);
-            final users = snapshot.data as List<Map<String, dynamic>>;
-            return ListView.builder(
-              itemCount: users.length,
-              itemBuilder: (context, index) {
-                final user = users[index];
-                return ListTile(
-                  title: Text(user['name']),
-                );
-              },
-            );
-          }
-          return const CircularProgressIndicator();
-        },
-      )),
+      body: Center(),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import flutter_appauth
 import path_provider_foundation
 import shared_preferences_foundation
 import sign_in_with_apple
-import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -19,6 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
