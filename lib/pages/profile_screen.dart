bde34386d4670e363515c84d59232fdff8a03fb9
quanming <116425066+wahpiangle@users.noreply.github.com>
2023-10-23 23:28:37 +0800

    added const & user image

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -24,16 +24,17 @@ class _ProfileScreenState extends State<ProfileScreen> {
 // nav bar
   int _selectedIndex = 2;
   final List<Widget Function()> pages = [
-        () => Home(),
-        () => const HistoryScreen(),
-        () => ProfileScreen(),
-        () => const ExercisesListScreen(),
-        () => const SettingsScreen(),
+    () => Home(),
+    () => const HistoryScreen(),
+    () => ProfileScreen(),
+    () => const ExercisesListScreen(),
+    () => const SettingsScreen(),
   ];
 
   void _onItemTapped(int index) {
     if (index >= 0 && index < pages.length) {
-      Navigator.of(context).push(MaterialPageRoute(builder: (context) => pages[index]()));
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => pages[index]()));
     }
   }
 // nav bar
@@ -44,82 +45,80 @@ class _ProfileScreenState extends State<ProfileScreen> {
     final user = Provider.of<User?>(context, listen: false);
 
     return Scaffold(
-
-      appBar: AppBar(
-        backgroundColor: Color(0xFF1A1A1A),
-        title: Text(
-          'Home',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF1A1A1A),
+          title: const Text(
+            'Home',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        centerTitle: true, // center title horizontally
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            // Will lead to Search friends page
-          },
-        ),
-      ),
-
-      body:
-      SingleChildScrollView(
-        child: Container(
-          color: Color(0xFF1A1A1A),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (user?.photoURL != null)
-                  ClipOval(
-                    child: Image.network(
-                      user?.photoURL ?? 'assets/defaultimage.jpg',
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
-                    ),
+          centerTitle: true, // center title horizontally
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              // Will lead to Search friends page
+            },
+          ),
+          actions: [
+            GestureDetector(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.network(
+                    user?.photoURL ?? 'assets/defaultimage.jpg',
+                    fit: BoxFit.cover,
                   ),
-                const SizedBox(height: 15),
-                Text(
-                  user?.displayName ?? '',
                 ),
-                //const SizedBox(height: 32),
-                //Start an Empty Widget
+              ),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            color: const Color(0xFF1A1A1A),
+            child: Center(
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
                 Padding(
-                  padding: EdgeInsets.only(bottom: 30),
+                  padding: const EdgeInsets.only(bottom: 30, top: 30),
                   // Start an Empty Workout button
-                  child: Container(
+                  child: SizedBox(
                     width: 340,
                     height: 60,
                     child: TextButton(
-                      onPressed: (){},
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(Color(0xFFC1C1C1)),
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30), // Set the border radius to make it round
-                            ),
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(const Color(0xFFC1C1C1)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                30), // Set the border radius to make it round
                           ),
                         ),
-                        child: Text(
-                            "Start an empty workout",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF1A1A1A),
-                            fontSize: 18,
-                          ),
+                      ),
+                      child: const Text(
+                        "Start an empty workout",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF1A1A1A),
+                          fontSize: 18,
                         ),
+                      ),
                     ),
                   ),
                 ),
-                SizedBox(height: 10),
+                const SizedBox(height: 10),
                 //Add a new template rectangle section
                 Container(
                   width: 340,
                   height: 150,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(28),
-                    color: Color(0xFF333333),
+                    color: const Color(0xFF333333),
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(18.0),
@@ -127,7 +126,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(
+                        const Text(
                           "Add new Template",
                           style: TextStyle(
                             color: Colors.white,
@@ -135,15 +134,17 @@ class _ProfileScreenState extends State<ProfileScreen> {
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        SizedBox(height:10),
-                        Text(
+                        const SizedBox(height: 10),
+                        const Text(
                           "Empower your journey with a new workout.",
                           style: TextStyle(
                             color: Color(0xFFC1C1C1),
                             fontSize: 13,
                           ),
                         ),
-                        SizedBox(height: 10), // Add some space between the two text widgets
+                        const SizedBox(
+                            height:
+                                10), // Add some space between the two text widgets
                         ElevatedButton(
                           onPressed: () {
                             // Navigator.push(
@@ -152,29 +153,32 @@ class _ProfileScreenState extends State<ProfileScreen> {
                             // );
                           },
                           style: ButtonStyle(
-                            minimumSize: MaterialStateProperty.all(Size(250, 40)),
-                            backgroundColor: MaterialStateProperty.all(Color(0xFFE1F0CF)),
-                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            minimumSize:
+                                MaterialStateProperty.all(const Size(250, 40)),
+                            backgroundColor: MaterialStateProperty.all(
+                                const Color(0xFFE1F0CF)),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
                               RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(40), // Set the border radius to make it round
+                                borderRadius: BorderRadius.circular(
+                                    40), // Set the border radius to make it round
                               ),
                             ),
                           ),
-                          child: Text(
-                              'Add New Template',
-                            style: TextStyle(
-                              color: Colors.black,
-                            )
-                          ),
+                          child: const Text('Add New Template',
+                              style: TextStyle(
+                                color: Colors.black,
+                              )),
                         ),
                       ],
                     ),
                   ),
                 ),
                 //End of Start a widget
-                SizedBox(width:0, height: 10),
+                const SizedBox(width: 0, height: 10),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 15),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 35, vertical: 15),
                   child: Column(
                     children: [
                       Row(
@@ -204,18 +208,15 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     ],
                   ),
                 )
-        ] //Children widgets
+              ] //Children widgets
+                  ),
             ),
+          ),
         ),
-     ),
-
-      ),
 //Nav Bar
-    bottomNavigationBar: CustomBottomNavigationBar(
-    currentIndex: _selectedIndex,
-    onItemTapped: _onItemTapped,
-    )
-    );
-
+        bottomNavigationBar: CustomBottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onItemTapped: _onItemTapped,
+        ));
   }
 }
