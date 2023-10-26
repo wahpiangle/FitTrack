537a637b83c30a0d45a5a1dc30afdb4c0cf59738
mmmchu <mchu1703@gmail.com>
2023-10-26 17:20:03 +0800

    -exerciselist with 5 dummy data

---
+++ lib/pages/exerciselistthemes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExerciseListThemes {
+  static const Color appBarBackground = Colors.black;
+  static const Color searchBarBackground = Colors.grey;
+  static const Color filterButtonColor = Colors.white;
+  static const Color circularIconColor = Color(0xFFE1F0CF);
+  static const Color listItemBorderColor = Colors.grey;
+  static const Color listItemTextColor = Colors.white;
+}
