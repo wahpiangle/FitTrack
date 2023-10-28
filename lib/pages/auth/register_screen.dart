1f2d5d5f1623bc6f9d045ef715012bbb0eb6e47d
Anderson Yeo <114915975+andersonnyeo@users.noreply.github.com>
2023-10-29 07:53:26 +0800

    Update register_screen.dart
    Finish designing the register page but no yet validate them

--- lib/pages/auth/register_screen.dart
+++ lib/pages/auth/register_screen.dart
@@ -1,15 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:group_project/services/auth_service.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
+  
 
   @override
   State<RegisterScreen> createState() => _RegisterScreenState();
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
+  // text field state
+  String email = '';
+  String password = '';
+  String error = '';
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFF1A1A1A),
+      // const Color(0xFF1A1A1C),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1A1A1A),
+        centerTitle: true,
+        elevation: 0.0,
+        title: const Text('Sign up'),
+
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'Email',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                )
+              ),
+
+
+              const SizedBox(height: 20.0),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'you@example.com',
+                  fillColor: Colors.white,
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width:2.0)
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.pink, width:2.0)
+                  ),
+                ),
+                  validator: (val) => val!.isEmpty ? 'Enter an invalid email' : null,
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  }
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                  'Password',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                  )
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: '******',
+                    fillColor: Colors.white,
+                    filled: true,
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width:2.0)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.pink, width:2.0)
+                    ),
+                  ),
+                  obscureText: true,
+                  validator: (val) => val!.length < 6 ? 'Enter a password 6 chars long' : null,
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  }
+              ),
+              const SizedBox(height: 30.0),
+              ElevatedButton(
+
+
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(color: Colors.white),
+
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      print(email);
+                      print(password);
+                      // dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                      // if(result == null){
+                      //   setState(() => error = 'please supply a valid email' );
+                      // }
+                    }
+                  }
+              ),
+              const SizedBox(height: 12.0),
+              Text(
+                error,
+                style: const TextStyle(color: Colors.red, fontSize: 14.0),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
