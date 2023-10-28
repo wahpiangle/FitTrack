9d2fd7847ff771f01e6e234ac035450d15db05ae
Anderson Yeo <114915975+andersonnyeo@users.noreply.github.com>
2023-10-28 18:57:44 +0800

    Update email_password_login.dart
    pre-design the log in page

--- lib/pages/auth/email_password_login.dart
+++ lib/pages/auth/email_password_login.dart
@@ -8,8 +8,57 @@ class EmailPasswordLogin extends StatefulWidget {
 }
 
 class _EmailPasswordLoginState extends State<EmailPasswordLogin> {
+
+  // text field state
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFF1A1A1A),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1A1A1A),
+        title: const Text('Sign in to Fit Connect'),
+        
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 20.0),
+              TextFormField(
+                onChanged: (val) {
+                  setState(() => email = val );
+
+                }
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() => password = val );
+
+                }
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                  child: const Text(
+                      'Sign in',
+                    style: TextStyle(color: Colors.white),
+
+                  ),
+                  onPressed: () async {
+                    print(email);
+                    print(password);
+
+                  }
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
