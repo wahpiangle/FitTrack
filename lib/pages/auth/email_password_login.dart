1477ae513f8fc5a5f4b19ee782b18bec6122153f
Anderson Yeo <114915975+andersonnyeo@users.noreply.github.com>
2023-10-29 07:53:14 +0800

    Update email_password_login.dart
    Finish designing the login page but no yet validate them

--- lib/pages/auth/email_password_login.dart
+++ lib/pages/auth/email_password_login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:group_project/services/auth_service.dart';
 
 class EmailPasswordLogin extends StatefulWidget {
   const EmailPasswordLogin({super.key});
@@ -9,52 +10,112 @@ class EmailPasswordLogin extends StatefulWidget {
 
 class _EmailPasswordLoginState extends State<EmailPasswordLogin> {
 
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
   // text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF1A1A1A),
+      // const Color(0xFF1A1A1C),
       appBar: AppBar(
         backgroundColor: const Color(0xFF1A1A1A),
-        title: const Text('Sign in to Fit Connect'),
-        
+        elevation: 0.0,
+        title: const Text('Log In'),
+        centerTitle: true,
+
+
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
+              const Text(
+                  'Email',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                  )
+              ),
+
+
               const SizedBox(height: 20.0),
               TextFormField(
-                onChanged: (val) {
-                  setState(() => email = val );
-
-                }
+                  decoration: const InputDecoration(
+                    hintText: 'you@example.com',
+                    fillColor: Colors.white,
+                    filled: true,
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width:2.0)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.pink, width:2.0)
+                    ),
+                  ),
+                  validator: (val) => val!.isEmpty ? 'Enter an invalid email' : null,
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  }
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                  'Password',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                  )
               ),
               const SizedBox(height: 20.0),
               TextFormField(
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() => password = val );
-
-                }
+                  decoration: const InputDecoration(
+                    hintText: '******',
+                    fillColor: Colors.white,
+                    filled: true,
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width:2.0)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.pink, width:2.0)
+                    ),
+                  ),
+                  obscureText: true,
+                  validator: (val) => val!.length < 6 ? 'Enter a password 6 chars long' : null,
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  }
               ),
-              const SizedBox(height: 20.0),
+              const SizedBox(height: 30.0),
               ElevatedButton(
+
+
                   child: const Text(
-                      'Sign in',
+                    'Log In',
                     style: TextStyle(color: Colors.white),
 
                   ),
                   onPressed: () async {
-                    print(email);
-                    print(password);
-
+                    if (_formKey.currentState!.validate()) {
+                      dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                      if(result == null){
+                        setState(() => error = 'please supply a valid email' );
+                      }
+                    }
                   }
-              )
+              ),
+              const SizedBox(height: 12.0),
+              Text(
+                error,
+                style: const TextStyle(color: Colors.red, fontSize: 14.0),
+              ),
+
             ],
           ),
         ),
