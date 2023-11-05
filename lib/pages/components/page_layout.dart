c7aa8e36f5b5c99c859a0c728fef842e41b1deae
stephanynene <110772101+stephanynene@users.noreply.github.com>
2023-11-05 19:47:53 +0800

    top app bar fix

--- lib/pages/components/page_layout.dart
+++ lib/pages/components/page_layout.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'top_nav_bar.dart';
 import 'bottom_nav_bar.dart';
+import 'package:provider/provider.dart';
 
 class PageLayout extends StatefulWidget {
   const PageLayout({super.key});
@@ -10,8 +12,26 @@ class PageLayout extends StatefulWidget {
 }
 
 class _PageLayoutState extends State<PageLayout> {
+  final int _currentIndex = 2; // Initialize the selected page index
+
+  //  page titles
+  final List<String> _pageTitles = [
+    'Home', // 0
+    'History', // 1
+    'Workout',
+    'Exercises',
+    'Settings',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final user = Provider.of<User?>(context);
+
+    return Scaffold(
+      appBar: TopNavBar(
+        title: _pageTitles[_currentIndex],
+        user: user,
+      ),
+    );
   }
 }
