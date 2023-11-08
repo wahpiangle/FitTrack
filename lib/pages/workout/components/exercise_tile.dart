34e678178e1d4662427bdfbc48c924304971cc62
quanming <116425066+wahpiangle@users.noreply.github.com>
2023-11-08 21:43:00 +0800

    fix padding

--- lib/pages/workout/components/exercise_tile.dart
+++ lib/pages/workout/components/exercise_tile.dart
@@ -35,50 +35,53 @@ class _ExerciseTileState extends State<ExerciseTile> {
         itemCount: widget.selectedExercises.length + 1,
         itemBuilder: (context, index) {
           if (widget.selectedExercises.isEmpty) {
-            return Column(children: [
-              Container(
-                alignment: Alignment.topLeft,
-                child: const Text(
-                  //TODO: Handle the workout title
-                  'snapshot.data!.title',
-                  style: TextStyle(
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(children: [
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: const Text(
+                    //TODO: Handle the workout title
+                    'snapshot.data!.title',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  style: const TextStyle(
                     color: Colors.white,
-                    fontSize: 24,
                     fontWeight: FontWeight.bold,
                   ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-                onChanged: (value) {
-                  //TODO: Handle the workout note
-                },
-                decoration: InputDecoration(
-                  hintText: 'Add a workout note',
-                  hintStyle: const TextStyle(
-                    color: Color(0xFFC1C1C1),
-                    fontWeight: FontWeight.bold,
-                  ),
-                  fillColor: const Color(0xFF333333),
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0),
+                  onChanged: (value) {
+                    //TODO: Handle the workout note
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Add a workout note',
+                    hintStyle: const TextStyle(
+                      color: Color(0xFFC1C1C1),
+                      fontWeight: FontWeight.bold,
+                    ),
+                    fillColor: const Color(0xFF333333),
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              AddExerciseButton(
-                exerciseData: widget.exerciseData,
-                selectedExercises: widget.selectedExercises,
-                selectExercise: widget.selectExercise,
-                removeExercise: widget.removeExercise,
-              ),
-              const CancelWorkoutButton(),
-            ]);
+                const SizedBox(height: 20),
+                AddExerciseButton(
+                  exerciseData: widget.exerciseData,
+                  selectedExercises: widget.selectedExercises,
+                  selectExercise: widget.selectExercise,
+                  removeExercise: widget.removeExercise,
+                ),
+                const CancelWorkoutButton(),
+              ]),
+            );
           }
           if (index == 0) {
             ExerciseSetInfo selectedExercise = ExerciseSetInfo.fromJson(
