2dec95d85102948ffec64c3d2e63409a4aad5bb0
stephanynene <110772101+stephanynene@users.noreply.github.com>
2023-11-10 20:03:26 +0800

    _updateContent method

--- lib/pages/components/page_layout.dart
+++ lib/pages/components/page_layout.dart
@@ -1,5 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../exercise/exercise_list.dart';
+import '../history_screen.dart';
+import '../home.dart';
+import '../settings_screen.dart';
+import '../workout/workout_screen.dart';
 import 'top_nav_bar.dart';
 import 'bottom_nav_bar.dart';
 import 'package:provider/provider.dart';
@@ -12,7 +17,7 @@ class PageLayout extends StatefulWidget {
 }
 
 class _PageLayoutState extends State<PageLayout> {
-  final int _currentIndex = 2; // Initialize the selected page index
+  late int _currentIndex = 2; // Initialize the selected page index to New Workout Screen
 
   //  page titles
   final List<String> _pageTitles = [
@@ -31,7 +36,34 @@ class _PageLayoutState extends State<PageLayout> {
       appBar: TopNavBar(
         title: _pageTitles[_currentIndex],
         user: user,
+      ), body: _buildBody(),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTabTapped: (index) {
+          // Update the selected page when a navigation item is tapped.
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
+  }
+
+  Widget _buildBody() {
+    // Switch  content based on the selected page index
+    switch (_currentIndex) {
+      case 0:
+        return const Home();
+      case 1:
+        return const HistoryScreen();
+      case 2:
+        return const WorkoutScreen();
+      case 3:
+        return const ExerciseListScreen();
+      case 4:
+        return const SettingsScreen();
+      default:
+        return Container(); // Handle other cases as needed
+    }
   }
 }
