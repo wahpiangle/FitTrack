35dbaa6df6904bdfb2003fe8e436ab83f6d08927
quanming <116425066+wahpiangle@users.noreply.github.com>
2023-11-11 15:05:21 +0800

    UI fixes to fill screen vertically

--- lib/pages/workout/workout_screen.dart
+++ lib/pages/workout/workout_screen.dart
@@ -31,161 +31,156 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
 
     return Scaffold(
       appBar: TopNavBar(title: 'Home', user: user),
-      body: SafeArea(
+      body: Container(
+        height: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        color: const Color(0xFF1A1A1A),
         child: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            color: const Color(0xFF1A1A1A),
-            child: Center(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 20),
-                    child: FractionallySizedBox(
-                      widthFactor: 1,
-                      child: TextButton(
-                        onPressed: () {
-                          objectBox.createWorkoutSession();
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => StartNewWorkout(
-                                exerciseData: exerciseData,
-                              ),
-                            ),
-                          );
-                        },
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                              const Color(0xFFC1C1C1)),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                          ),
-                          padding:
-                              MaterialStateProperty.all<EdgeInsetsGeometry>(
-                            const EdgeInsets.symmetric(vertical: 15),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: FractionallySizedBox(
+                  widthFactor: 1,
+                  child: TextButton(
+                    onPressed: () {
+                      objectBox.createWorkoutSession();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => StartNewWorkout(
+                            exerciseData: exerciseData,
                           ),
                         ),
-                        child: const Text(
-                          "Start An Empty Workout",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                            fontSize: 18,
-                          ),
+                      );
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(const Color(0xFFC1C1C1)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
                         ),
+                      ),
+                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                        const EdgeInsets.symmetric(vertical: 15),
+                      ),
+                    ),
+                    child: const Text(
+                      "Start An Empty Workout",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 18,
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 10, top: 10),
-                    child: FractionallySizedBox(
-                      widthFactor: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(28),
-                          color: const Color(0xFF333333),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.max,
-                            children: <Widget>[
-                              const Text(
-                                "Add new template",
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10, top: 10),
+                child: FractionallySizedBox(
+                  widthFactor: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(28),
+                      color: const Color(0xFF333333),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          const Text(
+                            "Add new template",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 23,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+                          const Text(
+                            "Empower your journey with a new workout.",
+                            style: TextStyle(
+                              color: Color(0xFFC1C1C1),
+                              fontSize: 13,
+                              letterSpacing: 0.4,
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+                          FractionallySizedBox(
+                            widthFactor: 1,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Add navigation logic for adding a new template.
+                              },
+                              style: ButtonStyle(
+                                minimumSize: MaterialStateProperty.all(
+                                    const Size(290, 40)),
+                                backgroundColor: MaterialStateProperty.all(
+                                    const Color(0xFFE1F0CF)),
+                                shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(40),
+                                  ),
+                                ),
+                              ),
+                              child: const Text(
+                                'Add New Template',
                                 style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 23,
+                                  color: Colors.black,
                                   fontWeight: FontWeight.bold,
+                                  letterSpacing: 0.3,
+                                  fontSize: 16,
                                 ),
                               ),
-                              const SizedBox(height: 15),
-                              const Text(
-                                "Empower your journey with a new workout.",
-                                style: TextStyle(
-                                  color: Color(0xFFC1C1C1),
-                                  fontSize: 13,
-                                  letterSpacing: 0.4,
-                                ),
-                              ),
-                              const SizedBox(height: 15),
-                              FractionallySizedBox(
-                                widthFactor: 1,
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    // Add navigation logic for adding a new template.
-                                  },
-                                  style: ButtonStyle(
-                                    minimumSize: MaterialStateProperty.all(
-                                        const Size(290, 40)),
-                                    backgroundColor: MaterialStateProperty.all(
-                                        const Color(0xFFE1F0CF)),
-                                    shape: MaterialStateProperty.all<
-                                        RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(40),
-                                      ),
-                                    ),
-                                  ),
-                                  child: const Text(
-                                    'Add New Template',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                      letterSpacing: 0.3,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
-                        ),
+                        ],
                       ),
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      NewWorkout(
-                        imagePath: 'assets/icons/dumbell.png',
-                        workoutText: 'Legs',
-                        exerciseData: exerciseData, // Add this line
-                      ),
-                      SizedBox(width: 15),
-                      NewWorkout(
-                        imagePath: 'assets/icons/dumbell.png',
-                        workoutText: 'Back',
-                        exerciseData: exerciseData, // Add this line
-                      ),
-                    ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  NewWorkout(
+                    imagePath: 'assets/icons/dumbell.png',
+                    workoutText: 'Legs',
+                    exerciseData: exerciseData, // Add this line
                   ),
-                  SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      NewWorkout(
-                        imagePath: 'assets/icons/dumbell.png',
-                        workoutText: 'Chest',
-                        exerciseData: exerciseData, // Add this line
-                      ),
-                      SizedBox(width: 15),
-                      NewWorkout(
-                        imagePath: 'assets/icons/dumbell.png',
-                        workoutText: 'Arms',
-                        exerciseData: exerciseData, // Add this line
-                      ),
-                    ],
+                  SizedBox(width: 15),
+                  NewWorkout(
+                    imagePath: 'assets/icons/dumbell.png',
+                    workoutText: 'Back',
+                    exerciseData: exerciseData, // Add this line
                   ),
                 ],
               ),
-            ),
+              SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  NewWorkout(
+                    imagePath: 'assets/icons/dumbell.png',
+                    workoutText: 'Chest',
+                    exerciseData: exerciseData, // Add this line
+                  ),
+                  SizedBox(width: 15),
+                  NewWorkout(
+                    imagePath: 'assets/icons/dumbell.png',
+                    workoutText: 'Arms',
+                    exerciseData: exerciseData, // Add this line
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
