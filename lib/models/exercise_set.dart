0105e0aa9dedfd6545210330b856f827a64b753d
quanming <116425066+wahpiangle@users.noreply.github.com>
2023-11-12 22:20:39 +0800

    add TODO comments

--- lib/models/exercise_set.dart
+++ lib/models/exercise_set.dart
@@ -11,6 +11,7 @@ class ExerciseSet {
 
   final exerciseSetInfo = ToOne<ExercisesSetsInfo>();
 
+  // TODO: make nullable to certain fields as some sets don't require weight or reps, maybe add a time field also
   ExerciseSet({
     this.id = 0,
     required this.weight,
