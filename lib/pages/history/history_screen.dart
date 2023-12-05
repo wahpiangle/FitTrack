b8c13070858a2522e7e01386e3edd771de6090c6
quanming <116425066+wahpiangle@users.noreply.github.com>
2023-12-05 23:45:57 +0800

    Update history_screen.dart

--- lib/pages/history/history_screen.dart
+++ lib/pages/history/history_screen.dart
@@ -14,47 +14,41 @@ class _HistoryScreenState extends State<HistoryScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-          ),
-          height: double.infinity,
-          color: const Color(0xFF1A1A1A),
-          child: StreamBuilder<List<WorkoutSession>>(
-            stream: objectBox.workoutSessionService.watchWorkoutSession(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else {
-                return snapshot.data!.isEmpty
-                    ? const Center(
-                        child: Text(
-                        'No workout sessions yet!',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      ))
-                    : ListView.builder(
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (context, index) {
-                          return WorkoutCard(
-                            workoutSession: snapshot.data![index],
-                          );
-                        },
-                      );
-              }
-            },
-          ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            objectBox.test();
+        height: double.infinity,
+        color: const Color(0xFF1A1A1A),
+        child: StreamBuilder<List<WorkoutSession>>(
+          stream: objectBox.workoutSessionService.watchWorkoutSession(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return snapshot.data!.isEmpty
+                  ? const Center(
+                      child: Text(
+                      'No workout sessions yet!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ))
+                  : ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        return WorkoutCard(
+                          workoutSession: snapshot.data![index],
+                        );
+                      },
+                    );
+            }
           },
-          child: const Icon(Icons.add),
-          backgroundColor: const Color(0xFF1A1A1A),
-        ));
+        ),
+      ),
+    );
   }
 }
