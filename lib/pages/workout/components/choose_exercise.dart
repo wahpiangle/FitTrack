8b90c1973702bff102315398cf6b8d51985d6281
stephanynene <110772101+stephanynene@users.noreply.github.com>
2023-12-19 17:58:34 +0800

    added usage of exercise_filter_page - non functional yet

--- lib/pages/workout/components/choose_exercise.dart
+++ lib/pages/workout/components/choose_exercise.dart
@@ -100,117 +100,115 @@ class _ChooseExerciseState extends State<ChooseExercise> {
       appBar: AppBar(
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios),
+          color: Colors.white,
           onPressed: () {
             Navigator.pop(context);
           },
         ),
-        title: const Text("Choose an Exercise"),
-        backgroundColor: const Color(0xFF1A1A1A),
+        title: const Text(
+            "Choose Exercises",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          )
+        ),
+        backgroundColor:  AppColours.primary,
       ),
       body: Container(
         color: const Color(0xFF1A1A1A),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                onChanged: filterExercises,
-                style: const TextStyle(color: Colors.white),
-                decoration: const InputDecoration(
-                  hintText: 'Search for an exercise...',
-                  hintStyle: TextStyle(color: Colors.white70),
-                  prefixIcon: Icon(Icons.search, color: Colors.white),
-                ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              ExercisesListFilters(
+                  filterExercises: filterExercises,
+                  selectedBodyPart: selectedBodyPart,
+                  setSelectedBodyPart: setSelectedBodyPart,
+                  selectedCategory: selectedCategory,
+                  addSelectedCategory: addSelectedCategory,
+                  removeSelectedCategory: removeSelectedCategory
               ),
-            ),
-            // ExercisesListFilters(
-            //     filterExercises: filterExercises,
-            //     selectedBodyPart: selectedBodyPart,
-            //     setSelectedBodyPart: setSelectedBodyPart,
-            //     selectedCategory: selectedCategory,
-            //     addSelectedCategory: addSelectedCategory,
-            //     removeSelectedCategory: removeSelectedCategory
-            // ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: filteredExercises.length,
-                itemBuilder: (context, index) {
-                  final exercise = filteredExercises[index];
-                  return Material(
-                    color: const Color(0xFF1A1A1A),
-                    child: InkWell(
-                      onTap: () {
-                        _selectExercise(exercise);
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(vertical: 5),
-                        child: ListTile(
-                          tileColor: exercise.isSelected ? Colors.grey[800] : null,
-                          horizontalTitleGap: -10,
-                          leading: SizedBox(
-                            height: 100,
-                            width: 100,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(300.0),
-                              child: exercise.imagePath == ''
-                                  ? Container(
-                                      decoration: BoxDecoration(
-                                        color: const Color(0xFFE1F0CF),
-                                        borderRadius:
-                                            BorderRadius.circular(300.0),
-                                      ),
-                                      child: Center(
-                                        child: Text(
-                                          exercise.name[0].toUpperCase(),
-                                          style: const TextStyle(
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 24.0,
+              Expanded(
+                child: ListView.builder(
+                  itemCount: filteredExercises.length,
+                  itemBuilder: (context, index) {
+                    final exercise = filteredExercises[index];
+                    return Material(
+                      color: const Color(0xFF1A1A1A),
+                      child: InkWell(
+                        onTap: () {
+                          _selectExercise(exercise);
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(vertical: 5),
+                          child: ListTile(
+                            tileColor: exercise.isSelected ? Colors.grey[800] : null,
+                            horizontalTitleGap: -10,
+                            leading: SizedBox(
+                              height: 100,
+                              width: 100,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(300.0),
+                                child: exercise.imagePath == ''
+                                    ? Container(
+                                        decoration: BoxDecoration(
+                                          color: const Color(0xFFE1F0CF),
+                                          borderRadius:
+                                              BorderRadius.circular(300.0),
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                            exercise.name[0].toUpperCase(),
+                                            style: const TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 24.0,
+                                            ),
                                           ),
                                         ),
+                                      )
+                                    : ClipPath(
+                                        clipper: MyClipperPath(),
+                                        child: Container(
+                                          height: 60,
+                                          width: 80,
+                                          decoration: BoxDecoration(
+                                              image: DecorationImage(
+                                            image: Image.asset(exercise.imagePath)
+                                                .image,
+                                            fit: BoxFit
+                                                .contain, //or whatever BoxFit you want
+                                          )),
+                                        ),
                                       ),
-                                    )
-                                  : ClipPath(
-                                      clipper: MyClipperPath(),
-                                      child: Container(
-                                        height: 60,
-                                        width: 80,
-                                        decoration: BoxDecoration(
-                                            image: DecorationImage(
-                                          image: Image.asset(exercise.imagePath)
-                                              .image,
-                                          fit: BoxFit
-                                              .contain, //or whatever BoxFit you want
-                                        )),
-                                      ),
-                                    ),
-                            ),
-                          ),
-                          title: Padding(
-                            padding: const EdgeInsets.only(bottom: 10),
-                            child: Text(
-                              exercise.name,
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
                               ),
                             ),
-                          ),
-                          subtitle: Text(
-                            exercise.bodyPart.target!.name,
-                            style: TextStyle(
-                              color: Colors.grey[500],
-                              fontSize: 14,
+                            title: Padding(
+                              padding: const EdgeInsets.only(bottom: 10),
+                              child: Text(
+                                exercise.name,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                            subtitle: Text(
+                              exercise.bodyPart.target!.name,
+                              style: TextStyle(
+                                color: Colors.grey[500],
+                                fontSize: 14,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       floatingActionButton: isAnyExerciseSelected
