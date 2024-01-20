33abd2645dfa7ac710544fd9f2a4f0c6ef9ec3aa
mmmchu <mchu1703@gmail.com>
2024-01-20 23:21:42 +0800

    user cant save if textfield empty

--- lib/pages/exercise/components/custom_exercise.dart
+++ lib/pages/exercise/components/custom_exercise.dart
@@ -11,6 +11,7 @@ class CustomExerciseDialog {
     String? customWorkoutName;
     String? selectedBodyPart;
     String? selectedCategory;
+    GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
     showDialog(
       context: context,
@@ -27,74 +28,88 @@ class CustomExerciseDialog {
           )
               : null,
           content: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 8.0),
-                  padding: EdgeInsets.symmetric(horizontal: 16.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
-                    color: Color(0xFF1A1A1A),
-                    border: Border.all(color: Color(0xFF1A1A1A)),
-                  ),
-                  child: TextField(
-                    onChanged: (value) {
-                      customWorkoutName = value;
-                    },
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      hintText: 'add name...',
-                      hintStyle: TextStyle(color: Colors.white),
-                      border: InputBorder.none,
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 8.0),
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: TextFormField(
+                      onChanged: (value) {
+                        customWorkoutName = value;
+                      },
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: 'add name...',
+                        hintStyle: TextStyle(color: Colors.white),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white), // Color of the underline
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white), // Color of the focused underline
+                        ),
+                        errorBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red), // Color of the error underline
+                        ),
+                        errorStyle: TextStyle(color: Colors.red),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a workout name';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                ),
-                SizedBox(height: 16.0),
-                DropdownButtonFormField<String>(
-                  value: selectedBodyPart,
-                  onChanged: (String? newValue) {
-                    selectedBodyPart = newValue;
-                  },
-                  items: bodyPartData.map<DropdownMenuItem<String>>((BodyPart bodyPart) {
-                    return DropdownMenuItem<String>(
-                      value: bodyPart.name,
-                      child: Text(
-                        bodyPart.name,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    );
-                  }).toList(),
-                  decoration: InputDecoration(
-                    labelText: 'Select Body Part',
-                    labelStyle: TextStyle(color: Colors.white),
-                    isDense: true,
-                    border: UnderlineInputBorder(borderSide: BorderSide.none),
+
+                  SizedBox(height: 16.0),
+                  DropdownButtonFormField<String>(
+                    value: selectedBodyPart,
+                    onChanged: (String? newValue) {
+                      selectedBodyPart = newValue;
+                    },
+                    items: bodyPartData.map<DropdownMenuItem<String>>((BodyPart bodyPart) {
+                      return DropdownMenuItem<String>(
+                        value: bodyPart.name,
+                        child: Text(
+                          bodyPart.name,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      );
+                    }).toList(),
+                    decoration: InputDecoration(
+                      labelText: 'Select Body Part',
+                      labelStyle: TextStyle(color: Colors.white),
+                      isDense: true,
+                      border: UnderlineInputBorder(borderSide: BorderSide.none),
+                    ),
+                    dropdownColor: Color(0xFF1A1A1A),
                   ),
-                  dropdownColor: Color(0xFF1A1A1A),
-                ),
-                DropdownButtonFormField<String>(
-                  value: selectedCategory,
-                  onChanged: (String? newValue) {
-                    selectedCategory = newValue;
-                  },
-                  items: categoryData.map<DropdownMenuItem<String>>((Category category) {
-                    return DropdownMenuItem<String>(
-                      value: category.name,
-                      child: Text(
-                        category.name,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    );
-                  }).toList(),
-                  decoration: InputDecoration(
-                    labelText: 'Select Category',
-                    labelStyle: TextStyle(color: Colors.white),
-                    isDense: false,
-                    border: UnderlineInputBorder(borderSide: BorderSide.none),
+                  DropdownButtonFormField<String>(
+                    value: selectedCategory,
+                    onChanged: (String? newValue) {
+                      selectedCategory = newValue;
+                    },
+                    items: categoryData.map<DropdownMenuItem<String>>((Category category) {
+                      return DropdownMenuItem<String>(
+                        value: category.name,
+                        child: Text(
+                          category.name,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      );
+                    }).toList(),
+                    decoration: InputDecoration(
+                      labelText: 'Select Category',
+                      labelStyle: TextStyle(color: Colors.white),
+                      isDense: false,
+                      border: UnderlineInputBorder(borderSide: BorderSide.none),
+                    ),
+                    dropdownColor: Color(0xFF1A1A1A),
                   ),
-                  dropdownColor: Color(0xFF1A1A1A),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           actions: [
@@ -135,27 +150,29 @@ class CustomExerciseDialog {
                   Expanded(
                     child: TextButton(
                       onPressed: () {
-                        String selectedBodyPartText =
-                        selectedBodyPart != null ? 'Selected Body Part: $selectedBodyPart' : 'No Body Part Selected';
-                        String selectedCategoryText =
-                        selectedCategory != null ? 'Selected Category: $selectedCategory' : 'No Category Selected';
-                        String customWorkoutNameText =
-                        customWorkoutName != null ? 'Custom Workout Name: $customWorkoutName' : 'No Workout Name Entered';
+                        if (formKey.currentState?.validate() ?? false) {
+                          String selectedBodyPartText =
+                          selectedBodyPart != null ? 'Selected Body Part: $selectedBodyPart' : 'No Body Part Selected';
+                          String selectedCategoryText =
+                          selectedCategory != null ? 'Selected Category: $selectedCategory' : 'No Category Selected';
+                          String customWorkoutNameText =
+                          customWorkoutName != null ? 'Custom Workout Name: $customWorkoutName' : 'No Workout Name Entered';
 
-                        print(selectedBodyPartText);
-                        print(selectedCategoryText);
-                        print(customWorkoutNameText);
+                          print(selectedBodyPartText);
+                          print(selectedCategoryText);
+                          print(customWorkoutNameText);
 
-                        // Create an Exercise object with the entered data
-                        Exercise newExercise = Exercise(
-                          name: customWorkoutName ?? '',
-                          bodyPart: selectedBodyPart ?? '', // Provide default value if null
-                          category: selectedCategory ?? '',
-                        );
+                          // Create an Exercise object with the entered data
+                          Exercise newExercise = Exercise(
+                            name: customWorkoutName ?? '',
+                            bodyPart: selectedBodyPart ?? '', // Provide default value if null
+                            category: selectedCategory ?? '',
+                          );
 
-                        // Use the ObjectBox instance to add the exercise to the list
-                        objectBox.addExerciseToList(newExercise);
-                        Navigator.pop(context);
+                          // Use the ObjectBox instance to add the exercise to the list
+                          objectBox.addExerciseToList(newExercise);
+                          Navigator.pop(context);
+                        }
                       },
                       style: TextButton.styleFrom(
                         padding: EdgeInsets.all(8.0),
@@ -180,9 +197,6 @@ class CustomExerciseDialog {
               ),
             ),
           ],
-
-
-
         );
       },
     );
