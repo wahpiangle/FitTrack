df495aa81c12cd8d8d5cebdc82fe436a9ac4aeae
mmmchu <mchu1703@gmail.com>
2024-01-31 01:16:05 +0800

    ontoggle visibility for exercise list item

--- lib/pages/exercise/components/exercise_list_item.dart
+++ lib/pages/exercise/components/exercise_list_item.dart
@@ -5,10 +5,10 @@ import 'package:group_project/pages/exercise/exercise_detail.dart';
 class ExerciseListItem extends StatelessWidget {
   final Exercise exercise;
   final String searchText;
-  final VoidCallback? onToggleVisibility;
+  final VoidCallback onToggleVisibility;
 
   const ExerciseListItem(
-      {super.key, required this.exercise, required this.searchText,this.onToggleVisibility});
+      {super.key, required this.exercise, required this.searchText,required this.onToggleVisibility});
 
   @override
   Widget build(BuildContext context) {
