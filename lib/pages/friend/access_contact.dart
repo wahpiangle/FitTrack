ba03eb48d1ddec982c591bf4e7750a337573ac01
chuien9797 <mytance9797@gmail.com>
2024-02-17 02:15:57 +0800

    Layout

--- lib/pages/friend/access_contact.dart
+++ lib/pages/friend/access_contact.dart
@@ -1,7 +1,5 @@
 import 'package:permission_handler/permission_handler.dart';
 
-// ...
-
 Future<void> requestContactsPermission() async {
   var status = await Permission.contacts.status;
   if (!status.isGranted) {
