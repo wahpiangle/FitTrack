b506039fd491f6385e90abce2df66c4bc821aad8
Quan Ming <116425066+wahpiangle@users.noreply.github.com>
2024-02-22 19:45:36 +0800

    refactor: remove unused files & add name to registration

--- lib/services/firebase/auth_service.dart
+++ lib/services/firebase/auth_service.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:group_project/main.dart';
+import 'package:group_project/services/firebase/firebase_user_service.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -17,7 +18,6 @@ class AuthService {
     return _auth.authStateChanges().map((User? user) => user);
   }
 
-
   //sign in with email and password
   Future signInWithEmailAndPassword(String email, String password) async {
     try {
@@ -33,12 +33,14 @@ class AuthService {
   }
 
   //register with email and password
-  Future registerWithEmailAndPassword(String email, String password) async {
+  Future registerWithEmailAndPassword(
+      String email, String password, String name) async {
     try {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       User? user = result.user;
       user?.sendEmailVerification();
+      FirebaseUserService.createUserInFirestore(user!, name);
       return user;
     } catch (e) {
       rethrow;
