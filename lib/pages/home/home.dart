0713b55eef55c8bc254958fb034cc044591c9025
mmmchu <mchu1703@gmail.com>
2024-02-22 09:54:32 +0800

    caption

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -7,7 +7,6 @@ import 'package:group_project/pages/home/components/display_image_stack.dart';
 import 'package:group_project/constants/themes/app_colours.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 class Home extends StatefulWidget {
   const Home({
     super.key,
@@ -43,6 +42,7 @@ class _HomeState extends State<Home> {
         context.read<UploadImageProvider>().setUploadError(true);
         context.read<UploadImageProvider>().setIsUploading(false);
       }
+
     });
   }
 
@@ -62,7 +62,6 @@ class _HomeState extends State<Home> {
           child: Column(
             children: [
               SizedBox(height: 20),
-              // Add space above the carousel
               // Display user info if there are images
               if (hasImages)
                 Row(
@@ -73,12 +72,11 @@ class _HomeState extends State<Home> {
                         CircleAvatar(
                           backgroundImage:
                           AssetImage('assets/icons/defaultimage.jpg'),
-                          // Add path to profile image
                           radius: 20,
                         ),
                         SizedBox(width: 10),
                         Text(
-                          'User_123456789', // Add the username
+                          'User_123456789',
                           style: TextStyle(
                             color: Colors.white,
                             fontSize: 16,
@@ -98,96 +96,114 @@ class _HomeState extends State<Home> {
                     ),
                   ],
                 ),
-              SizedBox(height: 10),
+              SizedBox(height: 2), // Reduce the vertical space here
               // Add some space between user info and carousel
-              CarouselSlider.builder(
-                itemCount: imageList.length,
-                options: CarouselOptions(
-                  aspectRatio: 0.9,
-                  enlargeCenterPage: true,
-                  enableInfiniteScroll: false,
-                  enlargeFactor: 0.2, // Adjust the enlargeFactor to increase the size
-                  viewportFraction: 0.9, // Adjust the viewportFraction for larger images
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      _current = index;
-                    });
+              SizedBox(
+                child: CarouselSlider.builder(
+                  itemCount: imageList.length,
+                  options: CarouselOptions(
+                    aspectRatio: 0.9,
+                    enlargeCenterPage: true,
+                    enableInfiniteScroll: false,
+                    enlargeFactor: 0.2,
+                    viewportFraction: 0.6,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _current = index;
+                      });
+                    },
+                  ),
+                  itemBuilder: (context, index, realIndex) {
+                    final firstImage = imageList[index]['firstImageUrl']!;
+                    final secondImage = imageList[index]['secondImageUrl']!;
+                    final postId = imageList[index]['postId']!;
+                    final workoutSessionId =
+                    imageList[index]['workoutSessionId']!;
+                    return Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 5),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius:
+                            const BorderRadius.all(Radius.circular(8.0)),
+                            child: DisplayImageStack(
+                              firstImageUrl: firstImage,
+                              secondImageUrl: secondImage,
+                              index: index,
+                              current: _current,
+                              postId: postId,
+                              workoutSessionId: workoutSessionId,
+                            ),
+                          ),
+                          uploadImageProvider.uploadError
+                              ? const Text(
+                            'There was an error uploading your workout. Please try again.',
+                            style: TextStyle(color: Colors.red),
+                            textAlign: TextAlign.center,
+                          )
+                              : Container(
+                            width: 300,
+                            padding: const EdgeInsets.symmetric(horizontal: 1, vertical: 1),
+                            decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              border: Border.all(color: Colors.transparent),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: TextField(
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                              enableInteractiveSelection: false, // Disables cursor and text selection
+                              decoration: InputDecoration(
+                                alignLabelWithHint: true, // Align the label with the hint
+                                hintText: 'Add a caption',
+                                hintStyle: TextStyle(color: Colors.grey),
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.only(left: 16), // Adjust left padding
+                              ),
+                              // Add your caption input logic here
+                            ),
+
+                          ),
+
+                          Container(
+                            width: 300,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 2, vertical: 2),
+                            decoration: BoxDecoration(
+                              color: Color(0xFF333333),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 4,
+                                  child: TextField(
+                                    style: TextStyle(color: Colors.white),
+                                    decoration: InputDecoration(
+                                      prefix: SizedBox(width: 10),
+                                      hintText: 'Add a comment...',
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none,
+                                    ),
+                                    // Handle comment logic
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    // Handle submit comment
+                                  },
+                                  icon: Icon(Icons.send),
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
                   },
                 ),
-
-                itemBuilder: (context, index, realIndex) {
-                  final firstImage = imageList[index]['firstImageUrl']!;
-                  final secondImage = imageList[index]['secondImageUrl']!;
-                  final postId = imageList[index]['postId']!;
-                  final workoutSessionId =
-                  imageList[index]['workoutSessionId']!;
-                  return Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 5),
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          borderRadius:
-                          const BorderRadius.all(Radius.circular(8.0)),
-                          child: DisplayImageStack(
-                            firstImageUrl: firstImage,
-                            secondImageUrl: secondImage,
-                            index: index,
-                            current: _current,
-                            postId: postId,
-                            workoutSessionId: workoutSessionId,
-                          ),
-                        ),
-                        uploadImageProvider.uploadError
-                            ? const Text(
-                          'There was an error uploading your workout. Please try again.',
-                          style: TextStyle(color: Colors.red),
-                          textAlign: TextAlign.center,
-                        )
-                            : const Text(
-                          // todo: allow user to add a caption to their post
-                          'Add a caption',
-                          style: TextStyle(color: Colors.white),
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(height: 20),
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                          decoration: BoxDecoration(
-                            color: Color(0xFF333333),
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                flex: 1, // Adjust flex to make TextField smaller
-                                child: TextField(
-                                  style: TextStyle(color: Colors.white), // Text color
-                                  decoration: InputDecoration(
-                                    prefix: SizedBox(width: 10),
-                                    hintText: 'Add a comment...',
-                                    hintStyle: TextStyle(color: Colors.grey), // Hint color
-                                    border: InputBorder.none,
-                                  ),
-                                  // Handle comment logic
-                                ),
-                              ),
-                              IconButton(
-                                onPressed: () {
-                                  // Handle submit comment
-                                },
-                                icon: Icon(Icons.send),
-                                color: Colors.white, // Send button color
-                              ),
-                            ],
-                          ),
-                        ),
-
-                      ],
-                    ),
-                  );
-                },
               ),
-
               // TODO: Display friend's posts
             ],
           ),
