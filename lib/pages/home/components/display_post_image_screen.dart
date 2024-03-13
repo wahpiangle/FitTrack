145b24717f0c0da6e8c7d15756b25f3924038a63
mmmchu <mchu1703@gmail.com>
2024-03-13 13:31:55 +0800

    date

--- lib/pages/home/components/display_post_image_screen.dart
+++ lib/pages/home/components/display_post_image_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:group_project/constants/themes/app_colours.dart';
 import 'package:group_project/models/post.dart';
 import 'package:group_project/pages/complete_workout/capture_image/components/interactive_image_viewer.dart';
+import 'package:intl/intl.dart'; // Import intl package for DateFormat
 
 class DisplayPostImageScreen extends StatelessWidget {
   final Post post;
@@ -9,29 +10,47 @@ class DisplayPostImageScreen extends StatelessWidget {
     super.key,
     required this.post,
   });
-  // TODO: display exercise details & comments
+
+  // Function to format the date
+  String formatDateTime(DateTime dateTime) {
+    return DateFormat('EEEE, hh:mm:ss a').format(dateTime);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'My Post',
-          style: TextStyle(color: Colors.white),
-        ),
         backgroundColor: AppColours.primary,
+        flexibleSpace: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'My Post',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 8),
+              Text(
+                '${formatDateTime(post.date)}',
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
         iconTheme: const IconThemeData(
           color: Colors.white,
         ),
       ),
       backgroundColor: AppColours.primary,
       body: Container(
-        margin: const EdgeInsets.symmetric(
-          horizontal: 10,
-        ),
-        height: MediaQuery.of(context).size.height * 0.5,
-        child: InteractiveImageViewer(
-          imagePath: post.firstImageUrl,
-          imagePath2: post.secondImageUrl,
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.5,
+          child: InteractiveImageViewer(
+            imagePath: post.firstImageUrl,
+            imagePath2: post.secondImageUrl,
+          ),
         ),
       ),
     );
