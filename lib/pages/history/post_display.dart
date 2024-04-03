d5a65bc2151461bcdebc8c62488f2133f9334031
mabel <mchu1703@gmail.com>
2024-04-03 13:12:31 +0800

    post image in history page

--- lib/pages/history/post_display.dart
+++ lib/pages/history/post_display.dart
@@ -1,11 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:group_project/models/firebase/comments.dart';
 import 'package:group_project/models/firebase/reaction.dart';
 import 'package:group_project/models/post.dart';
+import 'package:group_project/pages/home/components/display_post_screen/comment/comment_tile.dart';
 import 'package:group_project/pages/home/components/front_back_image.dart';
 import 'package:group_project/services/firebase/firebase_posts_service.dart';
+import 'package:group_project/services/firebase/firebase_comment_service.dart'; // Import the FirebaseCommentService
 import 'package:cached_network_image/cached_network_image.dart';
 
-class PostDisplay extends StatelessWidget {
+class PostDisplay extends StatefulWidget {
   final String postId;
   final bool isVisible;
 
@@ -16,16 +20,23 @@ class PostDisplay extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _PostDisplayState createState() => _PostDisplayState();
+}
+
+class _PostDisplayState extends State<PostDisplay> {
+  bool _showComments = false;
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         AnimatedOpacity(
-          opacity: isVisible ? 1.0 : 0.0,
+          opacity: widget.isVisible ? 1.0 : 0.0,
           duration: Duration(milliseconds: 500),
           curve: Curves.easeInOut,
           child: Transform.translate(
-            offset: isVisible ? Offset(0.0, 0.0) : Offset(0.0, 50.0),
+            offset: widget.isVisible ? Offset(0.0, 0.0) : Offset(0.0, 50.0),
             child: Align(
               alignment: Alignment.centerLeft,
               child: Padding(
@@ -52,11 +63,11 @@ class PostDisplay extends StatelessWidget {
           ),
         ),
         AnimatedOpacity(
-          opacity: isVisible ? 1.0 : 0.0,
+          opacity: widget.isVisible ? 1.0 : 0.0,
           duration: Duration(milliseconds: 500),
           curve: Curves.easeInOut,
           child: Transform.translate(
-            offset: isVisible ? Offset(0.0, 0.0) : Offset(0.0, 50.0),
+            offset: widget.isVisible ? Offset(0.0, 0.0) : Offset(0.0, 50.0),
             child: Container(
               height: MediaQuery.of(context).size.height * 0.35,
               width: MediaQuery.of(context).size.width * 1,
@@ -64,7 +75,7 @@ class PostDisplay extends StatelessWidget {
               child: Stack(
                 children: [
                   FutureBuilder<Post>(
-                    future: FirebasePostsService.getPostById(postId),
+                    future: FirebasePostsService.getPostById(widget.postId),
                     builder: (context, snapshot) {
                       if (snapshot.connectionState == ConnectionState.waiting) {
                         return Center(child: CircularProgressIndicator());
@@ -72,29 +83,32 @@ class PostDisplay extends StatelessWidget {
                         return Center(child: Text('Error: ${snapshot.error}'));
                       } else if (snapshot.hasData) {
                         final post = snapshot.data!;
-                        return ClipRRect(
+                        return post.firstImageUrl.isNotEmpty || post.secondImageUrl.isNotEmpty
+                            ? ClipRRect(
                           borderRadius: BorderRadius.circular(20),
                           child: Stack(
                             children: [
-                              Positioned(
-                                top: 0,
-                                left: 5,
-                                right: 0,
-                                child: getImageBasedonType(post.firstImageUrl, false),
-                              ),
-                              Positioned(
-                                top: 20,
-                                left: 20,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(5),
-                                  child: getImageBasedonType(post.secondImageUrl, true),
+                              if (post.firstImageUrl.isNotEmpty)
+                                Positioned(
+                                  top: 0,
+                                  left: 5,
+                                  right: 0,
+                                  child: getImageBasedonType(post.firstImageUrl, false),
                                 ),
-                              ),
+                              if (post.secondImageUrl.isNotEmpty)
+                                Positioned(
+                                  top: 20,
+                                  left: 20,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(5),
+                                    child: getImageBasedonType(post.secondImageUrl, true),
+                                  ),
+                                ),
                               Positioned(
                                 bottom: 40,
                                 right: 5,
                                 child: FutureBuilder<List<Reaction>>(
-                                  future: FirebasePostsService.getReactionsByPostId(postId),
+                                  future: FirebasePostsService.getReactionsByPostId(widget.postId),
                                   builder: (context, snapshot) {
                                     if (snapshot.connectionState == ConnectionState.waiting) {
                                       return CircularProgressIndicator();
@@ -131,23 +145,32 @@ class PostDisplay extends StatelessWidget {
                               ),
                             ],
                           ),
+                        )
+                            : Center(
+                          child: Text(
+                            'No posts yet',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
                         );
                       } else {
                         return Center(child: Text('No data available'));
                       }
                     },
                   ),
+
                   Positioned(
                     bottom: 0,
                     left: 0,
                     right: 0,
                     child: FutureBuilder<Post>(
-                      future: FirebasePostsService.getPostById(postId),
+                      future: FirebasePostsService.getPostById(widget.postId),
                       builder: (context, snapshot) {
                         if (snapshot.connectionState == ConnectionState.waiting) {
                           return CircularProgressIndicator();
-                        } else if (snapshot.hasError) {
-                          return Center(child: Text('Error: ${snapshot.error}'));
                         } else if (snapshot.hasData) {
                           final post = snapshot.data!;
                           if (post.caption.isNotEmpty) {
@@ -183,24 +206,86 @@ class PostDisplay extends StatelessWidget {
           ),
         ),
         AnimatedOpacity(
-          opacity: isVisible ? 1.0 : 0.0,
+          opacity: widget.isVisible ? 1.0 : 0.0,
           duration: Duration(milliseconds: 500),
           curve: Curves.easeInOut,
           child: Container(
-            child: TextButton(
-              onPressed: () {
-                // Handle onPressed action
+            child: FutureBuilder<Post>(
+              future: FirebasePostsService.getPostById(widget.postId),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (snapshot.hasData) {
+                  final post = snapshot.data!;
+                  return post.firstImageUrl.isNotEmpty || post.secondImageUrl.isNotEmpty
+                      ? TextButton( // Wrap TextButton in this condition
+                    onPressed: () {
+                      setState(() {
+                        _showComments = !_showComments;
+                      });
+                      print('postId: ${widget.postId}');
+                    },
+                    child: Text(
+                      _showComments ? 'Hide Comments' : 'View Comments',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                      ),
+                    ),
+                  )
+                      : SizedBox(); // Return an empty SizedBox if there are no posts
+                } else {
+                  return SizedBox(); // Return an empty SizedBox if no data available
+                }
               },
-              child: Text(
-                'View Comments',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                ),
-              ),
             ),
           ),
         ),
+
+        // Show comments if _showComments is true
+        // Inside PostDisplay widget
+        // Inside the PostDisplay widget where you use CommentTile
+        if (_showComments)
+          StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+            stream: FirebaseCommentService.getCommentStreamById(widget.postId),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
+                final comments = snapshot.data?.docs;
+                if (comments == null || comments.isEmpty) {
+                  return Center(child: Text('No comments available'));
+                } else {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: comments.length,
+                    itemBuilder: (context, index) {
+                      final commentData = comments[index].data();
+                      final comment = commentData['comment'] ?? '';
+                      final postedBy = commentData['postedBy'] ?? ''; // Get the user ID
+                      final date = commentData['date']; // Get the comment date
+
+                      return CommentTile(
+                        comment: Comment(
+                          id: '', // Provide an appropriate id value
+                          postId: widget.postId, // Pass the postId of the current post
+                          comment: comment,
+                          postedBy: postedBy,
+                          date: date != null ? DateTime.fromMillisecondsSinceEpoch(date.seconds * 1000) : DateTime.now(), // Handle null DateTime
+                        ),
+                      );
+                    },
+                  );
+                }
+              }
+            },
+          ),
+
       ],
     );
   }
