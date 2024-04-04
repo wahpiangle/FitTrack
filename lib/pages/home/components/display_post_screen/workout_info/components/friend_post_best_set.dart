104244f1e5008515fa4aae8167508189cdec3e0a
Anderson Yeo <114915975+andersonnyeo@users.noreply.github.com>
2024-04-04 16:09:28 +0800

    implement bestDurationSetInfo & bestRepsonlySetInfo
    this is hard coded as it is not shown in the friend's page yet

--- lib/pages/home/components/display_post_screen/workout_info/components/friend_post_best_set.dart
+++ lib/pages/home/components/display_post_screen/workout_info/components/friend_post_best_set.dart
@@ -15,10 +15,12 @@ class FriendPostBestSet extends StatelessWidget {
       final aOneRepMax = objectBox.exerciseService.getOneRepMaxValue(
         a.exerciseSets[0].weight ?? 0,
         a.exerciseSets[0].reps ?? 0,
+        a.exerciseSets[0], // Pass the ExerciseSet object
       );
       final bOneRepMax = objectBox.exerciseService.getOneRepMaxValue(
         b.exerciseSets[0].weight ?? 0,
         b.exerciseSets[0].reps ?? 0,
+        b.exerciseSets[0], // Pass the ExerciseSet object
       );
       return aOneRepMax > bOneRepMax ? a : b;
     });
@@ -26,13 +28,32 @@ class FriendPostBestSet extends StatelessWidget {
       final aOneRepMax = objectBox.exerciseService.getOneRepMaxValue(
         a.weight ?? 0,
         a.reps ?? 0,
+        a, // Pass the ExerciseSet object
       );
       final bOneRepMax = objectBox.exerciseService.getOneRepMaxValue(
         b.weight ?? 0,
         b.reps ?? 0,
+        b, // Pass the ExerciseSet object
       );
       return aOneRepMax > bOneRepMax ? a : b;
     });
+
+    final bestDurationSetInfo = workoutSession.exercisesSetsInfo.reduce((a, b) {
+      final aReps = a.exerciseSets[0].time ?? 0; // Change this to get the reps directly from the ExerciseSet
+      final bReps = b.exerciseSets[0].time ?? 0; // Change this to get the reps directly from the ExerciseSet
+      return aReps > bReps ? a : b;
+    });
+    final bestDurationSet = bestDurationSetInfo.exerciseSets.firstOrNull;
+
+
+    final bestRepsonlySetInfo = workoutSession.exercisesSetsInfo.reduce((a, b) {
+      final aReps = a.exerciseSets[0].reps ?? 0; // Change this to get the reps directly from the ExerciseSet
+      final bReps = b.exerciseSets[0].reps ?? 0; // Change this to get the reps directly from the ExerciseSet
+      return aReps > bReps ? a : b;
+    });
+    final bestRepsSet = bestRepsonlySetInfo.exerciseSets.firstOrNull;
+
+
     return Row(
       children: [
         const Icon(
@@ -52,19 +73,21 @@ class FriendPostBestSet extends StatelessWidget {
               ),
             ),
             Text(
-              bestExerciseSetInfo.exerciseName,
+              getBestExerciseName(bestSet, bestRepsSet, bestDurationSet, bestExerciseSetInfo.exerciseName),
               style: const TextStyle(
                 color: AppColours.secondary,
                 fontSize: 12,
               ),
             ),
+
             Text(
-              getWeightRepsText(bestSet),
+              getExerciseText(bestSet, bestRepsSet, bestDurationSet),
               style: const TextStyle(
                 color: AppColours.secondary,
                 fontSize: 12,
               ),
             ),
+
           ],
         )
       ],
@@ -92,5 +115,71 @@ String getWeightRepsText(ExerciseSet bestSet) {
 
   } else {
     return '${bestSet.weight} kg x ${bestSet.reps}';
+  }
+}
+
+String getDurationText(ExerciseSet? bestDurationSet) {
+  if (bestDurationSet == null) {
+    return ''; // No best set available
+  }
+  final timeString = bestDurationSet.time.toString();
+  if (timeString.length == 3) {
+    return '${timeString[0]}:${timeString[1]}${timeString[2]}';
+  } else if (timeString.length == 4) {
+    return '${timeString[0]}${timeString[1]}:${timeString[2]}${timeString[3]}';
+  } else if (timeString.length == 5) {
+    return '${timeString[0]}:${timeString[1]}${timeString[2]}:${timeString[3]}${timeString[4]}';
+  } else {
+    return '-'; // Invalid time format
+  }
+}
+
+String getRepsOnlyText(ExerciseSet? bestRepsSet) {
+  if (bestRepsSet == null) {
+    return ''; // No best set available
+  }
+  if (bestRepsSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Reps Only') {
+    return '${bestRepsSet.reps} reps';
+  } else {
+    return '-'; // Invalid time format
+  }
+}
+
+
+String getBestExerciseName(ExerciseSet bestSet, ExerciseSet? bestRepsSet, ExerciseSet? bestDurationSet, String bestExerciseName) {
+  if (bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Assisted Bodyweight' ||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Weighted Bodyweight' ||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Barbell'||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Dumbbell'||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Machine'||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Cable'||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Band'||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Other') {
+    return bestSet!.exerciseSetInfo.target!.exercise!.target!.name;
+  } else if (bestRepsSet?.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Reps Only') {
+    return bestRepsSet!.exerciseSetInfo.target!.exercise!.target!.name; // Adjust this line
+  } else if (bestDurationSet?.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Duration') {
+    return bestDurationSet!.exerciseSetInfo.target!.exercise!.target!.name; // Adjust this line
+  } else {
+    return '-';
+  }
+}
+
+String getExerciseText(ExerciseSet bestSet, ExerciseSet? bestRepsSet, ExerciseSet? bestDurationSet) {
+  if (bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Assisted Bodyweight' ||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Weighted Bodyweight' ||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Barbell'||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Dumbbell'||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Machine'||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Cable'||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Band'||
+      bestSet.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Other') {
+    return getWeightRepsText(bestSet);
+  } else if (bestRepsSet?.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Reps Only') {
+    return getRepsOnlyText(bestRepsSet!);
+  } else if (bestDurationSet?.exerciseSetInfo.target?.exercise.target?.category.target?.name == 'Duration') {
+    return getDurationText(bestDurationSet!);
+  } else {
+    return "-";
   }
 }
