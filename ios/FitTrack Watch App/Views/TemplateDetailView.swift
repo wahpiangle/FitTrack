a2c9f8d764cdedc548cab984d24fadd9ecf42fea
Quan Ming <116425066+wahpiangle@users.noreply.github.com>
2024-07-13 15:14:59 +0800

    Update TemplateDetailView.swift

--- ios/FitTrack Watch App/Views/TemplateDetailView.swift
+++ ios/FitTrack Watch App/Views/TemplateDetailView.swift
@@ -10,13 +10,51 @@ import SwiftUI
 struct TemplateDetailView: View {
     var template: WorkoutTemplate?
     var body: some View {
-        Image(template?.exerciseSetsInfo[0].exerciseImage ?? "arnold-press-muscles-1024x753-half")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 100, height: 100)
+        ScrollView{
+            VStack(alignment: .leading) {
+                Text(template?.title ?? "")
+                    .font(.headline)
+                Text(template?.note ?? "")
+                    .font(.subheadline)
+                    .foregroundColor(Color.gray)
+                Spacer()
+                ForEach(template?.exerciseSetsInfo ?? [], id: \.exerciseId) { exerciseSetInfo in
+                    HStack{
+                        VStack(alignment: .leading) {
+                            Text(exerciseSetInfo.exerciseName)
+                                .font(.headline)
+                            
+                        }
+                        Spacer()
+                        Image(template?.exerciseSetsInfo[0].exerciseImage ?? "")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 75, height: 75)
+                    }
+                }
+                Button(action:{}, label:
+                        {
+                    Text("Start Workout")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.black)
+                })
+                .tint(Color.theme.secondary)
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.roundedRectangle(radius: 10))
+                .controlSize(.mini)
+            }
+        }
     }
-}
+} 
 
 #Preview {
-    TemplateDetailView()
+    TemplateDetailView(
+        template: WorkoutTemplate(id: 1, title: "Test", note: "Test note", exerciseSetsInfo: [
+           ExerciseSetsInfo(exerciseId: 1, exerciseName: "Test name ex", exerciseImage: "belt-squat-muscles-1024x581-half", bodyPart: "Test bodypart", category: "category", sets: [
+               ExerciseSets(id: 1, reps: 1, weight: 1.0)
+           ]),
+           ExerciseSetsInfo(exerciseId: 1, exerciseName: "Test", exerciseImage: "belt-squat-muscles-1024x581-half", bodyPart: "Test", category: "Test", sets: [
+               ExerciseSets(id: 1, reps: 1, weight: 1.0)
+           ])
+       ]))
 }
